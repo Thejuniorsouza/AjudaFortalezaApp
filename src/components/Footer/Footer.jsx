2742b4c4c8cc1249f163d0dfebf6f7bd647ff0f3
Junior Souza <juniorsouzacmr.com>
2023-11-19 18:34:19 -0300

    ajuste navbar e footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,21 +8,21 @@ import back from "../../img/back.svg"
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <Navbar.Brand className="navbar-brand" href="index.html">
-                <img src={back} alt='logo'></img>
-                <h4 className="titleBrand" style={{ color: 'black' }}>AjudaFortaleza</h4>
+            <Navbar.Brand className={`d-flex align-items-sm-baseline justify-content-center`} href="index.html">
+                <img src={back} alt='logo' className='me-2'></img>
+                <h4 className="titleBrand fs-2 fw-bolder" style={{ color: 'black' }}>AjudaFortaleza</h4>
             </Navbar.Brand>
             <Nav bsPrefix='footerlinks' className="flex-grow-1 justify-content-evenly" >
-            <p className="textinformation" style={{ color: 'black' }}>Links</p>
-            <Nav.Link className={styles.navbar}  href="#home">HOME</Nav.Link>
-            <Nav.Link className={styles.navbar}  href="#linkListaDeOngs">LISTA DE ONGS</Nav.Link>
-            <Nav.Link className={styles.navbar}  href="#linkComoFunciona">COMO FUNCIONA</Nav.Link>
-            <Nav.Link className={styles.navbar}  href="#link">LINKS</Nav.Link>
+            <p className="fs-4 fw-bolder" style={{ color: 'black' }}>Links</p>
+            <Nav.Link className="fs-6 fw-normal"  href="#home">HOME</Nav.Link>
+            <Nav.Link className="fs-6 fw-normal"  href="#linkListaDeOngs">LISTA DE ONGS</Nav.Link>
+            <Nav.Link className="fs-6 fw-normal"  href="#linkComoFunciona">COMO FUNCIONA</Nav.Link>
+            <Nav.Link className="fs-6 fw-normal"  href="#link">LINKS</Nav.Link>
             </Nav>
             <Nav bsPrefix='footerlinks' className="flex-grow-1 justify-content-evenly" >
-                <p style={{ color: 'black' }}>Informações</p>
-            <Nav.Link className={styles.navbar}  href="#contato">(85)9.9999-9999</Nav.Link>
-            <Nav.Link className={styles.navbar}  href="#linkListaDeOngs">ajudafortaleza@gmail.com</Nav.Link>
+                <p className="fs-4 fw-bolder" style={{ color: 'black' }}>Informações</p>
+            <Nav.Link className="fs-6 fw-normal"  href="#contato">(85)9.9999-9999</Nav.Link>
+            <Nav.Link className="fs-6 fw-normal"  href="#linkListaDeOngs">ajudafortaleza@gmail.com</Nav.Link>
             </Nav>
             <span>© 2023 AjudaFortaleza. Todos os direitos reservados.</span>
         </footer>        
